2521400f5c1e7bd5177e0d88f926b7b5a4f970fb
Andrei <andreidanutmihai85@gmail.com>
2024-08-20 14:22:33 +0300

    port changed

--- server.js
+++ server.js
@@ -53,7 +53,7 @@ app.use((err, req, res, next) => {
 });
 
 // Start the server
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 10000;
 const server = app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
