e68879865b52cb5574c0b43757c7c6ed1fea02d9
Andrei <andreidanutmihai85@gmail.com>
2024-08-20 13:05:23 +0300

    crud

---
+++ config/configs.js
@@ -0,0 +1,9 @@
+// Set the connection string based from the config vars of the production server
+// To run locally use 'mongodb://localhost/mern-crud' instead of process.env.DB
+
+module.exports = {
+  db: 'mongodb+srv://serviceit:tFqDPSgpX9zjCutK@servicedb.zztkbj6.mongodb.net/',
+  react_app_url: "http://localhost:4200"
+
+  
+};
