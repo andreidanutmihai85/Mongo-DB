89e8327e7d8a27430ae40bde2ecdf202bba5b6ab
Andrei <andreidanutmihai85@gmail.com>
2024-08-20 17:45:33 +0300

    altul

--- config/configs.js
+++ config/configs.js
@@ -1,10 +1,10 @@
 // Set the connection string based from the config vars of the production server
 // To run locally use 'mongodb://localhost/mern-crud' instead of process.env.DB
 
+
 module.exports = {
   db: 'mongodb+srv://serviceit:tFqDPSgpX9zjCutK@servicedb.zztkbj6.mongodb.net/',
   react_app_url: "0.0.0.0:5000"
  
  
 };
-PORT=5000
